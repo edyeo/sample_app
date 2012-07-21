01c5feea9497e72a862b49ed3b10cf1990a9eee7
edyeo <karmaday9@gmail.com>
2012-07-21 20:22:31 +0900

    Make a basic User Model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,13 +4,30 @@ describe "Static pages" do
 
   subject { page  }
 
-  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: full_title(page_title)) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+  end
 
   describe "Home page" do
     before { visit root_path }
-    
-      it { should have_selector('h1', text: 'Sample App') } 
-      it { should have_selector('title', text: full_title(''))}
+      let(:heading) { 'Sample App' }
+      let(:page_title) { '' }
+
+      it_should_behave_like "all static pages"
       it { should_not have_selector 'title', text: '| Home' }
   end
 
