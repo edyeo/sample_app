aa89f0ff3193dac0969d4708be4e202214ee4a4a
edyeo <karmaday9@gmail.com>
2012-07-21 20:23:17 +0900

    Make a basic User Model (including secure passwords)

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -11,8 +11,5 @@ describe ApplicationHelper do
       full_title("foo").should =~ /^Ruby on Rails Tutorial Sample App/
     end
 
-    it "should not include a bar for the home page" do
-      full_title("").should_not =~ /\|/
-    end
   end
 end
