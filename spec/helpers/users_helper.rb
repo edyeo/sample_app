0578f4e78d34757baef13317a9a86c0bfb1c9e86
edyeo <karmaday9@gmail.com>
2012-07-22 13:28:56 +0900

    Finish user signup

--- spec/helpers/users_helper.rb
+++ spec/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
-  # Returns the Gravatar (http://gravatar.com/) for the given user.
+
+  # Returns the Gravatar
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    gravatar_url = "https://securer.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
-   end
-
+  end
 end
